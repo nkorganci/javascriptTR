1eb86473d42b6cd6479f4ae621572bb9245f0655
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-06-14 23:17:49 -0400

    11 17

--- ch2_decision/app.js
+++ ch2_decision/app.js
@@ -35,4 +35,87 @@
 // alert(a>18&&h=="yes" ? "good":"bad");
 // const rsl = a>18&&h=="yes"? console.log("accepted"):console.log("rejected");// can be assigned to a variable
 
-//--->> Switch Case
+//--------------->> SWITCH CASE
+// it checks value and type '==='
+// const day=Number(prompt("Write the day number"));//promt takes String automatically
+// var a;2
+// switch(day){
+//     case 1:
+//         dayn="monday";
+//         a="monday2";
+//         //break;
+//     case 2:
+//         dayn="tuesday";
+//         break;
+//     default:
+//         alert("not a valid num");
+//         break;
+// }
+
+// console.log(dayn);
+// console.log(a);
+
+//------->>> TERNARY OPERATORS
+
+var a = 22;
+var b = a > 120 ? "fast 😢" : a > 90 ? "normal 👍" : "slow 👏";
+console.log(b);
+
+//Example-Grading
+a = "a";
+
+switch (a) {
+  case "a":
+    console.log("a>90");
+    break;
+  case "b":
+    console.log("a>70");
+}
+
+//---------->>ITERATIONS, for, for in, fo of , foreach, while, do while
+for(let i=0; i<5;i++){
+    console.log("hello")
+};
+
+for(let i=0; i<5;i++){
+    console.log(i)
+};
+
+for(let i=0; i<5;i++){
+    console.log(`Name${i}`)
+};
+
+//--->>do-while
+// var a;
+// do{
+//     a=prompt("write a number");
+
+// }while(a<0||a>100);
+// console.log(a);
+
+//--->>>Primary Number
+var a=5;
+for(var b=2;b<a;b++){
+    if(a%b==0){
+        console.log("not prime");
+        break;
+    }else{
+        console.log("prime");
+        break;
+    }
+
+}
+
+//--->>primary number 2
+
+var a=8
+var b=true;
+for(var c=2;c<a;c++){
+    if(a%c==0){
+        b=false;
+        break;
+    }
+}
+var d= b?"primary":"not primary";
+console.log(d);
+
