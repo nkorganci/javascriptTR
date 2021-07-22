bf0569090d03f49cd452a5747b40298da6b06626
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-07-21 21:14:00 -0500

    7 21

--- ch4_DOM/ex03_domManipulation/app.js
+++ ch4_DOM/ex03_domManipulation/app.js
@@ -1,13 +1,6 @@
-//*querySelector can be used as: ".class_name" , "#id"
-
-function calculate() {
-  const r = document.getElementById("r").value;
-  const h = document.querySelector(".h").value;
-  const result = (Math.PI * r * r * h).toFixed(2);
-  console.log(result, r, h);
-  document.querySelector(".volume").innerHTML = result;
+document.querySelector(".on").onclick=function(){
+  document.querySelector(".pic").src="./img/b.png"
 }
-
-const button = document.querySelector(".btn");
-button.style.color = "red";
-button.style.backgroundColor = "yellow";
+document.querySelector(".off").onclick=function(){
+  document.querySelector(".pic").src="./img/a.png"
+}
