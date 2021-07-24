acb4ee673ff67e4b151b8017d304afa51650cdee
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-07-24 12:39:12 -0500

    m

--- ch05_arrays/ex03_iteration/iteration.js
+++ ch05_arrays/ex03_iteration/iteration.js
@@ -18,3 +18,108 @@ console.log(countN, countP);
 console.log(`positive ${countP}, negetive ${countN}`);
 
 //* For Of Arrays, Strings, Maps, can be use in all
+
+//? Example: Sort the animals
+/* const animals = ["cat", "dog", "bird", "fish", "cat"];
+const search = prompt("Search the animal").toLowerCase();
+
+const returnA = function (searchs) {
+  let count = 0;
+  for (let i = 0; i < animals.length; i++) {
+    if (search == animals[i]) {
+      count++;
+    }
+  }
+  return count;
+};
+const res = returnA(search);
+res == 0
+  ? console.log("Could not find")
+  : console.log(`The animal ${search} ${res}`); */
+
+//? Example: For of
+const addition = [2, 3, -2];
+let sum = 0;
+for (let i of addition) {
+  sum += i;
+}
+console.log(sum);
+
+const addition1 = [2, 3, -2];
+let sum1 = 0;
+for (let i in addition1) {
+  sum1 += addition1[i];
+}
+console.log(sum1);
+
+//? Array.forEach(), slower than for or for in, for of
+//* it writes all elements of array
+let numbers = [2, 3, "Hi"];
+numbers.forEach(write);
+
+//* each numbers goes to write function.
+function write(persons) {
+  console.log(persons);
+}
+
+//* 2nd Way, arrow function
+
+numbers.forEach((t) => console.log(t));
+
+//*Example: find the sum of the elements and write it in HTML
+
+//* 1.Way: Classic
+let num01 = [2, 4, 5];
+let sum01 = 0;
+for (let i = 0; i < num01.length; i++) {
+  sum01 += num01[i];
+}
+console.log(sum01);
+
+//* 2.Way: For in
+
+let num02 = [2, 4, 5];
+let sum02 = 0;
+for (let i in num02) {
+  sum02 += num02[i];
+}
+console.log(sum02);
+
+//* 3.Way: for of
+let num03 = [2, 4, 5];
+let sum03 = 0;
+for (let i of num03) {
+  sum03 += i;
+}
+console.log(sum03);
+
+//* 4.Way , forEach and write it in HTML
+let num04 = [2, 4, 6];
+let sum04 = 0;
+
+num04.forEach(calculate);
+
+function calculate(x) {
+  sum04 += x;
+  document.querySelector(".sum").innerHTML = sum04;
+}
+
+//* 5.Way arrow funtion and write in HTML
+
+let num05 = [2, 4, 6];
+let sum05 = 0;
+
+num05.forEach((t) => (sum05 += t));
+console.log(sum05);
+document.querySelector(".sum05").innerHTML = `${sum05} TL`;
+
+//* forEach( realValue, index, array);
+
+let num06 = [2, 4, 6];
+let sum06 = 0;
+
+num05.forEach((t,i,d) =>{
+  sum06 += t;
+} 
+console.log(sum06);
+document.querySelector(".sum05").innerHTML = `${sum06} TL`;
