d36980006d8136f1670b9662ee292c47b35e6983
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-08-01 23:03:09 -0500

    d

--- ch7_projects/pr01_countdown_timer/script.js
+++ ch7_projects/pr01_countdown_timer/script.js
@@ -1,7 +1,7 @@
 const daysEl = document.getElementById("days");
 const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
-const secondsEl = document.getElementById("seconds");
+const secondsEl=document.getElementById("seconds");
 
 const newYears = "1 Jan 2023";
 function countdown() {
@@ -14,11 +14,17 @@ function countdown() {
   const mins = Math.floor(totalSeconds / 60) % 60;
   const seconds = Math.floor(totalSeconds) % 60;
 
-  console.log(days, hours, minutes, seconds);
+    daysEl.innerHTML=days;
+    hoursEl.innerHTML=hours;
+    minsEl.innerHTML=mins;
+    secondsEl.innerHTML=seconds;
+
 }
 
 //Initial call
 countdown();
 
 setInterval(countdown, 1000);
+
+
  
