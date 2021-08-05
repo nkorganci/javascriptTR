ea4eaea68aea0f3ba55a6257a2335e01cbf75730
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-08-05 10:27:40 -0500

    a

--- ch9_JS_Summary/L02_EventListener/app.js
+++ ch9_JS_Summary/L02_EventListener/app.js
@@ -2,7 +2,13 @@ const grandParent = document.querySelector(".grandparent");
 const parent = document.querySelector(".parent");
 const child = document.querySelector(".child");
 
-grandParent.addEventListener("click", (e) => {
-  console.log(e.target);
+grandParent.addEventListener("click", e => {
+  console.log("grandparent1");
 });
+grandParent.addEventListener("click", e => {
+  console.log("grandparent2");
+});
+
+
+
 
