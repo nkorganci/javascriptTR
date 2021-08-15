2b8a54eef5bc361e27434413394e18ab0d5615b0
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-08-15 11:57:49 -0500

    a

--- ch9_JS_SummaryTutorials/L02_EventListener/app.js
+++ ch9_JS_SummaryTutorials/L02_EventListener/app.js
@@ -1,14 +1,71 @@
-const grandParent = document.querySelector(".grandparent");
+const grandparent = document.querySelector(".grandparent");
 const parent = document.querySelector(".parent");
 const child = document.querySelector(".child");
+console.log("Hi");
 
-grandParent.addEventListener("click", e => {
-  console.log("grandparent1");
+/* 
+//! Only one of them will work
+grandparent.addEventListener("click", (e) => {
+  e.stopPropagation();
+  console.log("GRand pare 1");
 });
-grandParent.addEventListener("click", e => {
-  console.log("grandparent2");
+parent.addEventListener("click", (e) => {
+  e.stopPropagation();
+  console.log("pare 2");
+});
+child.addEventListener("click", (e) => {
+  e.stopPropagation();
+  console.log("child 2");
+}); */
+
+/* //! CAPTURE METHOD
+grandparent.addEventListener(
+  "click",
+  (e) => {
+    console.log("GRand pare 1 capture");
+  },
+  { capture: true }
+);
+parent.addEventListener(
+  "click capture",
+  (e) => {
+    console.log("pare 2");
+  },
+  { capture: true }
+);
+child.addEventListener(
+  "click capture",
+  (e) => {
+    console.log("child 2");
+  },
+  { capture: true }
+);
+child.addEventListener(
+  "click capture",
+  (e) => {
+    console.log("child 2");
+  },
+  { once: true }
+);
+ */
+/* //! BUBBLE METHOD
+grandparent.addEventListener("click", (e) => {
+  console.log("GRand pare 1 BUBBLE");
+});
+parent.addEventListener("click", (e) => {
+  console.log("pare 2 BUBBLE");
+});
+child.addEventListener("click", (e) => {
+  console.log("chilD BUBBLE");
 });
 
+ */
 
+//! Adding new element
+const divs = document.querySelectorAll("div");
 
-
+divs.forEach((div) => {
+  div.addEventListener("click", () => {
+    console.log("before new element");
+  });
+});
