2c5d85ab1f7b3b27be916925f4525e7218ef95d3
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-08-15 16:27:54 -0500

    a

--- ch10_PracticeProjects/T0-DO List/app.js
+++ ch10_PracticeProjects/T0-DO List/app.js
@@ -1,8 +1,55 @@
-//Selectors
+// ? ========  1 SELECTORS =====================================================
 const todoInput = document.querySelector(".todo-input");
-const todobutton = document.querySelector(".todo-button");
+const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 
-//Event Listeners
+// ? ====== 2 Event Listeners ===============================================
 
-//Functions
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
+
+//? =====    3 Functions =====================================================
+// ? ADD TODO
+function addTodo(event) {
+  // Prevent refreshing page
+  event.preventDefault();
+
+  // adding todo Div
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+
+  //Create "li"
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+
+  //Checkmark button
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = "<i class= 'fas fa-check'></i>";
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+
+  //Chec trash button
+  const trashbutton = document.createElement("button");
+  trashbutton.innerHTML = "<i class= 'fas fa-trash'></i>";
+  trashbutton.classList.add("trash-btn");
+  todoDiv.appendChild(trashbutton);
+
+  //Append to the list
+  todoList.appendChild(todoDiv);
+
+  //Clear input value
+  todoInput.value = "";
+}
+
+//? DELETE CHECK
+function deleteCheck(e) {
+  const item = e.target;
+
+  // Delete TODO
+  if (item.classList[0] === "trash-btn") {
+    const todo = item.parentElement;
+    todo.remove();
+  }
+}
