162ed793992ea47965203f483a189d065de1f5c0
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-08-15 16:45:44 -0500

    a

--- ch10_PracticeProjects/T0-DO List/app.js
+++ ch10_PracticeProjects/T0-DO List/app.js
@@ -50,6 +50,17 @@ function deleteCheck(e) {
   // Delete TODO
   if (item.classList[0] === "trash-btn") {
     const todo = item.parentElement;
-    todo.remove();
+    //Animation
+    todo.classList.add("fall");
+    todo.addEventListener("transitionend", function () {
+      todo.remove();
+    });
+  }
+
+  //? CHECK MARK
+
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
   }
 }
