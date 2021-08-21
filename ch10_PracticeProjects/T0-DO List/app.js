574cd6768acd99b770886b004f50a1a2be885ecc
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-08-21 18:08:00 -0500

    a

--- ch10_PracticeProjects/T0-DO List/app.js
+++ ch10_PracticeProjects/T0-DO List/app.js
@@ -2,11 +2,13 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
 
 //* ====================  EVENT LISTENERS ================================
 
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("click", filterTodo);
 
 //* ====================  FUNCTIONS =======================================
 function addTodo(e) {
@@ -45,8 +47,28 @@ function addTodo(e) {
 
 function deleteCheck(e) {
   const item = e.target;
+
+  //DELETE TODO
   //! Why classList has an array and need to be equal to ...
   if (item.classList[0] === "trash-btn") {
-    todoInput.remove();
+    // item.remove();
+    const todo = item.parentElement;
+
+    //Animation
+    todo.classList.add("fall");
+    todo.addEventListener("transitionend", function () {
+      todo.remove();
+    });
   }
+
+  //Check Mark
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+  }
+}
+
+function filterTodo(e) {
+  const todos = todoList.childNodes;
+  console.log(todos);
 }
