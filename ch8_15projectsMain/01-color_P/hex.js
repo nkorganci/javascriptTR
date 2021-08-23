394f2716ad53261aa3eb9797847337394938f859
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-08-23 12:44:17 -0500

    a

--- ch8_15projectsMain/01-color_P/hex.js
+++ ch8_15projectsMain/01-color_P/hex.js
@@ -1,1 +1,26 @@
+//Selectors
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.querySelector("#btn");
+const color = document.querySelector(".color");
+
+//Events
+btn.addEventListener("click", background);
+
+// Functions
+function background() {
+  const r = randomColorSelect();
+  document.body.style.backgroundColor = r;
+  color.innerHTML = r;
+  btn.style.backgroundColor = r;
+  //   color.style.backgroundColor = r;
+  color.style.color = r;
+}
+
+function randomColorSelect() {
+  let randomC = "#";
+  for (let i = 0; i < 6; i++) {
+    randomN = Math.floor(Math.random() * hex.length);
+    randomC += hex[randomN];
+  }
+  return randomC;
+}
