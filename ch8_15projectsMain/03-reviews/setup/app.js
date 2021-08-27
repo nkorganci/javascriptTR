fe35ff68bc5cb6575bbdd07c0ca67c3a80aa8bbd
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-08-27 10:55:01 -0500

    a

--- ch8_15projectsMain/03-reviews/setup/app.js
+++ ch8_15projectsMain/03-reviews/setup/app.js
@@ -2,28 +2,28 @@
 const reviews = [
   {
     id: 1,
-    name: "susan smith",
+    name: "1susan smith",
     job: "web developer",
     img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
     text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
   },
   {
     id: 2,
-    name: "anna johnson",
+    name: "2anna johnson",
     job: "web designer",
     img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
     text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
   },
   {
     id: 3,
-    name: "peter jones",
+    name: "3peter jones",
     job: "intern",
     img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
     text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
   },
   {
     id: 4,
-    name: "bill anderson",
+    name: "4bill anderson",
     job: "the boss",
     img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
     text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
@@ -33,26 +33,53 @@ const reviews = [
 //Selectors
 const previous = document.querySelector(".previous");
 const next = document.querySelector(".next");
-const surprise = document.querySelector(".surprise");
+const suprise = document.querySelector(".suprise");
 
 const img = document.querySelector(".img");
 const author = document.querySelector(".author");
 const job = document.querySelector(".job");
 const text = document.querySelector(".text");
 
-//Events and functions
+//Set starting item
+let currentItem = 0;
 
-let person = reviews[0];
+//Load initial item (! what is the function of this function)
 
-const r = function(){
-  for (let i=0;i<reviews.length;i++){
-    
-  }
+window.addEventListener("DOMContentLoader", function () {
+  const item = reviews[currentItem];
+  img.src = item.img;
+  author.innerHTML = item.name;
+  job.textContent = item.job;
+  text.innerHTML = item.text;
+});
+
+//Show person based on item
+function showPerson(person) {
+  const item = reviews[person];
+  img.src = item.img;
+  author.innerHTML = item.name;
+  job.textContent = item.job;
+  text.innerHTML = item.text;
 }
 
+//show next person
 next.addEventListener("click", function () {
-  img.src = person.img;
-  author.innerHTML = person.name;
-  job.textContent = person.job;
-  text.innerHTML = person.text;
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
+  showPerson(currentItem);
+});
+
+previous.addEventListener("click", function () {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  showPerson(currentItem);
+});
+
+suprise.addEventListener("click", function () {
+  currentItem = Math.floor(Math.random() * reviews.length);
+  showPerson(r);
 });
