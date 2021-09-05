48cf03eedbe4a4727631cb58c6ca3bf1590d2428
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-09-05 12:08:01 -0500

    a

--- ch11_WebDevSimplified/simpleCal02/app.js
+++ ch11_WebDevSimplified/simpleCal02/app.js
@@ -1,27 +1,35 @@
-let display = document.getElementById('display');
+let display = document.querySelector("#display");
 
-let buttons = Array.from(document.getElementsByClassName('button'));
+// let button = document.querySelectorAll(".button"); return HTML collection/ node list
+// console.log(button);// Return HTML
 
-buttons.map( button => {
-    button.addEventListener('click', (e) => {
-        switch(e.target.innerText){
-            case 'C':
-                display.innerText = '';
-                break;
-            case '=':
-                try{
-                    display.innerText = eval(display.innerText);
-                } catch {
-                    display.innerText = "Error"
-                }
-                break;
-            case '←':
-                if (display.innerText){
-                   display.innerText = display.innerText.slice(0, -1);
-                }
-                break;
-            default:
-                display.innerText += e.target.innerText;
+let button = Array.from(document.querySelectorAll(".button"));
+// console.log(button);//Return array
+
+button.map((button) => {
+  button.addEventListener("click", (e) => {
+    /*     console.log("clicked");
+    console.log(e);
+    console.log(e.target);
+    console.log(e.target.innerText); */
+
+    switch (e.target.innerText) {
+      case "C":
+        display.innerText = "";
+        break;
+      case "←":
+        if (display.innerText) {
+          display.innerText = display.innerText.slice(0, -1);
         }
-    });
+      case "=":
+       try{
+          display.innerText = eval(display.innerText); 
+       }catch{
+           display.innerText="Error";
+       }
+        break;
+      default:
+        display.innerText += e.target.innerText;
+    }
+  });
 });
---
+++ ch11_WebDevSimplified/simpleCal03/app.js
@@ -0,0 +1,6 @@
+const display = document.querySelector(".display");
+const left = document.querySelector(".left");
+const equal = document.querySelector(".equal");
+const clear = document.querySelector(".clear");
+const numbers=document.querySelectorAll(".button");
+
