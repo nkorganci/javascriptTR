54d9d91d7c4c3306dd7099bef763e7bf13a09ccf
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-09-05 17:04:03 -0500

    a

--- ch11_WebDevSimplified/simpleCal03/app.js
+++ ch11_WebDevSimplified/simpleCal03/app.js
@@ -2,5 +2,26 @@ const display = document.querySelector(".display");
 const left = document.querySelector(".left");
 const equal = document.querySelector(".equal");
 const clear = document.querySelector(".clear");
-const numbers=document.querySelectorAll(".button");
+const numbers=Array.from(document.querySelectorAll(".button"));
 
+numbers.map((t)=>{
+    t.addEventListener("click",(e)=>{
+        switch(e.target.innerText){
+            case "C":
+                display.innerText="";
+                break;
+            case "&#8592;":
+                display.innerText=display.innerText.slice(0,-1);
+            case "=":
+                try{
+                   display.innerText=eval(display.innerText); 
+                }catch{
+                    display.innerText="Error";
+                }
+                break;
+            default:
+                display.innerText+=e.target.innerText;
+
+        }
+    });
+});
