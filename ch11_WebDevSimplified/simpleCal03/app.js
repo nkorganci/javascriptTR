ecd7f775ae3f2c03f8a9f9947db53bdbf320c5e0
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-09-06 09:29:48 -0500

    a

--- ch11_WebDevSimplified/simpleCal03/app.js
+++ ch11_WebDevSimplified/simpleCal03/app.js
@@ -1,17 +1,17 @@
 const display = document.querySelector(".display");
-const left = document.querySelector(".left");
-const equal = document.querySelector(".equal");
-const clear = document.querySelector(".clear");
 const numbers=Array.from(document.querySelectorAll(".button"));
 
-numbers.map((t)=>{
-    t.addEventListener("click",(e)=>{
+numbers.map((numbers)=>{
+    numbers.addEventListener("click",(e)=>{
         switch(e.target.innerText){
             case "C":
                 display.innerText="";
                 break;
             case "&#8592;":
-                display.innerText=display.innerText.slice(0,-1);
+                if(display.innerText){
+                    display.innerText=display.innerText.slice(0,-1);
+                };
+                
             case "=":
                 try{
                    display.innerText=eval(display.innerText); 
