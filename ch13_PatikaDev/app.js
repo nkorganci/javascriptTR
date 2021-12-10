724cb276dd08884f480775278d9ec9a6cc751736
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-12-09 20:11:37 -0600

    a

---
+++ ch13_PatikaDev/app.js
@@ -0,0 +1,9 @@
+// var c = 'Welcome to 10 Days of JavaScript1!';
+var parameterVariable = 'Welcome to 10 Days of JavaScript!';
+function greeting(parameterVariable) {
+    // This line prints 'Hello, World!' to the console:
+    console.log('Hello, World!');
+    // Write a line of code that prints parameterVariable to stdout using console.log:
+    console.log(parameterVariable);
+      
+}
