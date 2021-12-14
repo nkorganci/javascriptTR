18f6ac888356916440469e38c5a39480dcbbed53
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-12-14 06:08:09 -0600

    a

--- ch13_PatikaDev/02_DOMIleCalismak/app.js
+++ ch13_PatikaDev/02_DOMIleCalismak/app.js
@@ -1,8 +1,21 @@
-console.log(document.location.hostname)
-console.log(document.location.pathname)
+// console.log(document.location.hostname)
+// console.log(document.location.pathname)
 
-console.log(document.body)
-console.log(document.head)
-console.log(document.url)
+// console.log(document.body)
+// console.log(document.head)
+// console.log(document.url)
 
-console.log(document.body.style.backgroundColor="yellow")
+// console.log(document.body.style.backgroundColor="yellow")
+
+// document.write("write method");
+// document.writeln("writeln ")
+// document.write("new write")
+
+//Accessing field value by document object
+//   let h1 = document.getElementById('title');
+//   console.log(h1)
+//   h1.innerHTML="converted";
+
+//qUERY SELECTRO
+    
+let link = document.querySelector("")
