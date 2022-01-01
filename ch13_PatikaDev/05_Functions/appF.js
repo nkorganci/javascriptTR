428e8c07bf0cee9c5cd38f26118e5aca8ad760bc
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2022-01-01 09:13:07 -0600

    a

--- ch13_PatikaDev/05_Functions/appF.js
+++ ch13_PatikaDev/05_Functions/appF.js
@@ -1,24 +1,24 @@
 // 1. Function with parameter
-let firstName="Lorem";
+let firstName = "Lorem";
 
-function greetings(firstName1){
-  console.log(`Merhaba ${firstName1? firstName1: "No name"}`);
+function greetings(firstName1) {
+  console.log(`Merhaba ${firstName1 ? firstName1 : "No name"}`);
 }
 
 greetings("Enver");
 
 // 2. Functin with parameter 2
 
-function f2(n2="Enver2"){
+function f2(n2 = "Enver2") {
   console.log(`Merhaba ${n2}`);
 }
 
 f2("Enver3");
 
 //3. Function with global parameter
-let n3="Bera3";
+let n3 = "Bera3";
 
-function f3 (){
+function f3() {
   console.log(`Merhaba ${n3}`);
 }
 
@@ -26,9 +26,40 @@ f3();
 
 // 3. Function with parameters
 
-function f4(fn, ln){
+function f4(fn, ln) {
   let info = `Merhaba ${fn} ${ln}`;
   return info;
 }
 
-console.log(f4("Enver","Bera"));
+console.log(f4("Enver", "Bera"));
+
+//4. Task : factorial function
+
+    //Ist way
+
+// let n = prompt("Enter a number: ");
+// let result = 1;
+// for (let i = 1; i <= n; i++) {
+//   result *= i;
+// }
+
+// console.log(result);
+
+    //2nd Way
+
+    function factorial(n){
+      if(n==0){
+        return 1;
+      }else{
+        return n*factorial(n-1);
+      }
+    }
+
+   console.log(factorial(5)) ;
+
+    //3rd Way
+    function factorialize(n){
+      return n;
+    }
+    console.log(factorialize(4));
+   
