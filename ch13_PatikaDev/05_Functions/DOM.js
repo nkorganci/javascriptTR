d2a2d2b0ec0c26eb2042b2f0ee599b7adacc1d4a
Nuri Korganci <nkorganci@gmail.com>
2022-01-20 19:31:27 -0600

    a

--- ch13_PatikaDev/05_Functions/DOM.js
+++ ch13_PatikaDev/05_Functions/DOM.js
@@ -1,2 +1,1 @@
-const h1=document.querySelector(".h1");
-h1.addEventListener()
+var readline = require("readline");
---
+++ ch13_PatikaDev/06_DOM/app.js
@@ -0,0 +1,4 @@
+let h2 = document.getElementsByTagName("h2");
+console.log(h2);
+let p = document.querySelector("#demo");
+p.innerHTML= h2.innerHTML;
