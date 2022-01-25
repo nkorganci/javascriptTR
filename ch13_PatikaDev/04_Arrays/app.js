25485a849f4a7b4ed3b8428e8fa7dbabc6c601e5
Nuri Korganci <nkorganci@gmail.com>
2022-01-25 08:19:35 -0600

    m

--- ch13_PatikaDev/04_Arrays/app.js
+++ ch13_PatikaDev/04_Arrays/app.js
@@ -5,7 +5,12 @@ items.unshift(22)
 items.shift()
 console.log(items);
 
-let items1 = [1,2,3];
-items1.splice(0,2,234444,4)
+let items1 = ["a","b","ca"];
+items1.splice(0,2,234,4)
 
 console.log(items1);
+
+console.log(items1.includes(4));
+
+console.log(items1.join("/"));
+console.log(items1.concat(items));
